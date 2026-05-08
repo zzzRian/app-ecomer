396428a359be1063f8b05ae5798e90014e0f8002
POSTPERU <posperu2025@gmail.com>
2026-05-08 02:33:04 -0500

    fix railway env vars

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-change-me")
 
     # MYSQL RAILWAY
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
-    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("MYSQLUSER")
+    DB_PASSWORD = os.getenv("MYSQLPASSWORD")
+    DB_HOST = os.getenv("MYSQLHOST")
+    DB_PORT = os.getenv("MYSQLPORT")
+    DB_NAME = os.getenv("MYSQLDATABASE")
 
     # Seguridad para caracteres especiales en password
     DB_PASSWORD_SAFE = quote_plus(DB_PASSWORD) if DB_PASSWORD else ""
